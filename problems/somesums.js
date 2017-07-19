d59f61e2e5c7c04b3bc9407adc8339954ac549d3
Jerry Krusinski <jkruzr@gmail.com>
2017-07-19 10:11:53 -0500

    adds description

--- problems/somesums.js
+++ problems/somesums.js
@@ -1,3 +1,9 @@
+/*
+  Given an array of numbers and a desired sum,
+  return the earliest-starting subarray of length num whose entries
+  add up to sum from the input array. Return false if no such subarray exists
+*/
+
 function someSums(array, sum, num, sub = [], i = 0) {
   if (num === 0) {
     return sub.reduce((acc, next) => acc + next) === sum ? sub : false;
